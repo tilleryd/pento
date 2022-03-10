b80a11993cbf15ccefcbd48d9f138c03dd43abb5
David Tillery <2581080+tilleryd@users.noreply.github.com>
2022-03-10 11:58:08 -0500

    wire up wrong_live

---
+++ lib/pento_web/live/wrong_live.ex
@@ -0,0 +1,39 @@
+defmodule PentoWeb.WrongLive do
+  use Phoenix.LiveView, layout: {PentoWeb.LayoutView, "live.html"}
+
+  def mount(_params, _session, socket) do
+    {:ok, assign(socket, score: 0, message: "Make a guess:", time: time())}
+  end
+
+  def render(assigns) do
+    ~H"""
+    <h1>Your score: <%= @score %></h1>
+    <h2>
+      <%= @message %> It's <%= @time %>
+    </h2>
+    <h2>
+      <%= for n <- 1..10 do %>
+      <a href="#" phx-click="guess" phx-value-number={n} ><%= n %></a> <% end %>
+    </h2>
+    """
+  end
+
+  def time() do
+    DateTime.utc_now |> to_string
+  end
+
+  # function that changes the live view's state based on the inbound event
+  def handle_event("guess", %{"number" => guess}=_data, socket) do
+    message = "Your guess: #{guess}. Wrong. Guess again. "
+    score = socket.assigns.score - 1
+    {
+      :noreply,
+      assign(
+        socket,
+        message: message,
+        score: score,
+        time: time()
+      )
+    }
+  end
+end
